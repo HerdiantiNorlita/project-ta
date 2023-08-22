066c0aa077a15e68914c061888c7bccc9947a5c9
Herdianti Norlita <90747445+HerdiantiNorlita@users.noreply.github.com>
2023-08-22 22:18:16 +0700

    P

--- system/routes/api.php
+++ system/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ApiSensorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+	 Route::controller(ApiSensorController::class)->group(function () {
+        Route::get('sensor', 'index');
+        Route::post('sensor/save', 'store');
+   });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+
